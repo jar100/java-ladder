b979d46a347e717ab3ebb687ffa721abbd4c6724
jar100 <jar10038@gmail.com>
2018-09-18 14:24:57 +0900

    LadderGame step1 push

--- src/main/java/laddergame/LadderGame.java
+++ src/main/java/laddergame/LadderGame.java
@@ -1,5 +1,6 @@
 package laddergame;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,8 +8,8 @@ public class LadderGame {
     public static void main(String[] args) {
         int person = inputPeple();
         int height = inputHeight();
-        String[] lines = makeHeight(person, height);
-        getHeight(lines);
+        ArrayList<ArrayList<Boolean>> ladders = getHeight(person, height);
+        displayLadder(ladders);
     }
 
     public static int inputPeple() {
@@ -23,17 +24,13 @@ public class LadderGame {
         return scanner.nextInt();
         }
 
-    public static void resultView(int peples, int height) {
-        System.out.println("인간수 " + peples + " 사다리 높이 " + height);
-    }
-
     public static boolean isTrue() {
         Random random = new Random();
         int randomDraw = random.nextInt(10);
         return randomDraw >= 5;
     }
 
-    public static String lineOrblank(boolean isTrue) {
+    public static String lineOrBlank(boolean isTrue) {
         String line = " ";
         if(isTrue) {
             line = "-";
@@ -41,29 +38,37 @@ public class LadderGame {
         return line;
     }
 
-    public static String[] makeHeight(int person, int height) {
-        String[] lines = new String[height];
-        for (int i = 0; i < lines.length; i++) {
-            lines[i] = getOneLine(person);
+    public static void displayLadder(ArrayList<ArrayList<Boolean>> ladders) {
+        for (ArrayList<Boolean> ladder : ladders) {
+            toStringLadder(ladder);
         }
-        return lines;
+    }
+    public static void toStringLadder(ArrayList<Boolean> isLadder) {
+        String ladder = "";
+        for (Boolean aBoolean : isLadder) {
+            ladder += "l";
+            ladder += lineOrBlank(aBoolean);
+        }
+        ladder += "l";
+        System.out.println(ladder);
     }
 
     // l- 를 동시에 출력하다가 마지막만 l 출력
-    public static String getOneLine(int person) {
-        String line = "";
-        for (int i = 0; i < person; i++) {
-            line += "l";
-            if( i != (person-1)) {
-                line += lineOrblank(isTrue());
-            }
+    public static ArrayList<Boolean> getOneLine(int person) {
+        ArrayList<Boolean> ladder = new ArrayList<>();
+
+        for (int i = 0; i < person-1; i++) {
+            ladder.add(isTrue());
         }
-        return line;
+        return ladder;
     }
 
-    public static void getHeight(String[] lines) {
-        for (String line : lines) {
-            System.out.println(line);
+    public static ArrayList<ArrayList<Boolean>> getHeight(int person, int height) {
+        ArrayList<ArrayList<Boolean>> ladders = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            ladders.add(getOneLine(person));
         }
+        return ladders;
     }
+
 }
--- src/test/java/laddergame/LadderGameTest.java
+++ src/test/java/laddergame/LadderGameTest.java
@@ -2,6 +2,8 @@ package laddergame;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderGameTest {
@@ -15,21 +17,41 @@ public class LadderGameTest {
 
     @Test
     public void makeLine() {
-        String line = "";
-        line = LadderGame.getOneLine(4);
-        assertThat(line).isEqualTo("l-l l-l");
+        ArrayList<Boolean> line  = LadderGame.getOneLine(5);
+        for (int i=0; i < line.size(); i++) {
+            System.out.println(line.get(i));
+        }
+        assertThat(line.get(0)).isEqualTo(true);
     }
 
     @Test
-    public void line() {
-        String line = LadderGame.lineOrblank(true);
+    public void isLine() {
+        String line = LadderGame.lineOrBlank(true);
 
         assertThat(line).isEqualTo("-");
     }
     @Test
-    public void blank() {
-        String line = LadderGame.lineOrblank(false);
+    public void isBlank() {
+        String line = LadderGame.lineOrBlank(false);
 
         assertThat(line).isEqualTo(" ");
+    }
+
+    @Test
+    public void ladder() {
+        ArrayList<Boolean> line  = LadderGame.getOneLine(5);
+        LadderGame.toStringLadder(line);
+    }
+
+    @Test
+    public void getHeight() {
+        LadderGame.getHeight(3,4);
+    }
+
+    @Test
+    public void display() {
+        ArrayList<ArrayList<Boolean>> ladders = LadderGame.getHeight(3,4);
+        LadderGame.displayLadder(ladders);
+
     }
 }
